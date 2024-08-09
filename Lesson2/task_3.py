07738e9436d3910ab0bd8b1e504a98541685e9f3
lenakvt <kilovataya.e.n@gmail.com>
2024-08-09 22:59:35 +0400

    Homework2

--- Lesson2/task_3.py
+++ Lesson2/task_3.py
@@ -1,3 +1,6 @@
-def square (s)
-     return
-square (input("Введите сторону: "))
+def findSquare (a):
+     return a*a
+
+storonaStroka = input("Введите сторону квадрата: ")
+storonaNum = int(storonaStroka)
+print("результат:", findSquare(storonaNum))
---
+++ Lesson2/task_4.py
@@ -0,0 +1,12 @@
+def Fizz_Buzz(n):
+    if (n%3==0) and (n%5==0):
+        print ("FizzBazz")
+    elif (n%3==0):
+        print ("Fizz")
+    elif (n%5==0):
+        print ("Bazz")
+        
+number = input("Введите число: ")
+number= int(number)
+
+Fizz_Buzz(number)
