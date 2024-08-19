8726aa721ba870e222c813a815bb80557d49032d
lenakvt <kilovataya.e.n@gmail.com>
2024-08-19 11:26:15 +0400

    Flake 8 Homework3

--- Lesson3/task_2.py
+++ Lesson3/task_2.py
@@ -1,13 +1,12 @@
 from smartphone import Smartphone
 
-catalog=[
-    Smartphone("Apple", "iPhone 15","+79991112123"),
-    Smartphone("Huawei","nova 11 Pro", "+79992221213"),
-    Smartphone("Xiaomi","13 Pro","+79993132321"),
-    Smartphone("ASUS","Zenfone 10","+79994142432"),
-    Smartphone("POCO","F5 Pro","+79995453543")
+catalog = [
+    Smartphone("Apple", "iPhone 15", "+79991112123"),
+    Smartphone("Huawei", "nova 11 Pro", "+79992221213"),
+    Smartphone("Xiaomi", "13 Pro", "+79993132321"),
+    Smartphone("ASUS", "Zenfone 10", "+79994142432"),
+    Smartphone("POCO", "F5 Pro", "+79995453543")
 ]
 
 for phone in catalog:
-    phone.infoPhone()
-    
+    phone.infoPhone()    
--- Lesson3/task_3.py
+++ Lesson3/task_3.py
@@ -1,8 +1,8 @@
 from address import Address
 from mailing import Mailing
 
-to_address=Address(123456, "Sankt-Peterburg", "Prospect Toreza", 45, 50)
-from_address=Address(165432, "Batumi", "Lermontova", 50, 65)
-my_mailing=Mailing(to_address, from_address, 5000,"Отправление")
+to_address = Address(123456, "Sankt-Peterburg", "Prospect Toreza", 45, 50)
+from_address = Address(165432, "Batumi", "Lermontova", 50, 65)
+my_mailing = Mailing(to_address, from_address, 5000, "Отправление")
 
 my_mailing.departure()
