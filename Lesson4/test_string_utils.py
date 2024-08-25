9b579a62d71f73827a84c24353c4acec786f5256
lenakvt <kilovataya.e.n@gmail.com>
2024-08-25 20:48:05 +0400

    Add from 6 to 9 tests

--- Lesson4/test_string_utils.py
+++ Lesson4/test_string_utils.py
@@ -4,8 +4,6 @@ from string_utils import StringUtils
 utils = StringUtils()
 
 # Проверки capitilize
-
-
 @pytest.mark.parametrize("stringValue, result", [("skypro", "Skypro"), ("скайпро", "Скайпро"), ("555", "555")])
 def test_capitilize_positive(stringValue, result):
     assert utils.capitilize(stringValue) == result
@@ -32,7 +30,7 @@ def test_trim_negative(stringValue, result):
 # Проверки to_list
 @pytest.mark.parametrize("input, delimetr, result", [
     ("father,mother,daughter,son", ",", [
-     "father", "mother", "daughter", "son"]),
+        "father", "mother", "daughter", "son"]),
     ("5:4:3:2", ":", ["5", "4", "3", "2"]),
     ("", ",", []),
 ])
@@ -62,29 +60,111 @@ def test_contains_positive(input, value, result):
 
 @pytest.mark.xfail
 @pytest.mark.parametrize("input, value, result", [
-    ("Container","N", True),
-    ("Container","", False),
-    (" "," ", False)
+    ("Container", "N", True),
+    ("Container", "", False),
+    (" ", " ", False)
 ])
 def test_contains_negative(input, value, result):
     assert utils.contains(input, value) == result
-    
 
-#Проверки для Delete
-@pytest.mark.parametrize("input, value, result",[
-    ("Container", "Con","tainer"),
-    ("Container","ta","Coniner"),
-    ("Container123","er", "Contain123")
+
+# Проверки для Delete
+@pytest.mark.parametrize("input, value, result", [
+    ("Container", "Con", "tainer"),
+    ("Container", "ta", "Coniner"),
+    ("Container123", "er", "Contain123")
 ])
 def test_delete_positive(input, value, result):
     assert utils.delete_symbol(input, value) == result
-    
-    
+
+
 @pytest.mark.xfail
 @pytest.mark.parametrize("input, value, result", [
-    ("Container", "Con","Tainer"),
-    ("Container","Ta","Coniner"),
-    ("None","4", "Non")
+    ("Container", "Con", "Tainer"),
+    ("Container", "Ta", "Coniner"),
+    ("None", "4", "Non")
 ])
 def test_delete_negative(input, value, result):
-    assert utils.delete_symbol(input, value) == result   
+    assert utils.delete_symbol(input, value) == result
+
+
+# Проверки starts_with
+@pytest.mark.parametrize("input, value, result", [
+    ("Container", "C", True),
+    ("Container", "T", False),
+    ("123", "123", True)
+])
+def test_starts_with_positive(input, value, result):
+    assert utils.starts_with(input, value) == result
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("input, value, result", [
+    (" Container", "C", True),
+    ("Container", "1", True),
+    (" 555", "555", True)
+])
+def test_starts_with_negative(input, value, result):
+    assert utils.starts_with(input, value) == result
+
+
+# Проверки end_with
+@pytest.mark.parametrize("input, value, result", [
+    ("Container", "r", True),
+    ("Container", "n", False),
+    ("12345", "5", True)
+])
+def test_end_with_positive(input, value, result):
+    assert utils.end_with(input, value) == result
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("input, value, result", [
+    ("Container", "R", True),
+    ("Container", "9", True),
+    ("Container ", "r", True)
+])
+def test_end_with_negative(input, value, result):
+    assert utils.end_with(input, value) == result
+
+
+# Проверки is_empty
+@pytest.mark.parametrize("input, result", [
+    ("", True),
+    (" ", True),
+    ("Barometr", False)
+])
+def test_is_empty_positive(input, result):
+    assert utils.is_empty(input) == result
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("input, result", [
+    (" ", False),
+    ("0", True),
+    ("None", True)
+])
+def test_is_empty_negative(input, result):
+    assert utils.is_empty(input) == result
+
+
+# Проверки list_to_string
+@pytest.mark.parametrize("list, joiner, result", [
+    (["father", "mother", "daughter", "son"], ",", "father,mother,daughter,son"),
+    (["father", "mother", "daughter", "son"],
+     ", ", "father, mother, daughter, son"),
+    ([5, 4, 3, 2], ",", "5,4,3,2"),
+    (["Rostov", "Na", "Dony"], "-", "Rostov-Na-Dony")
+])
+def test_list_to_string_positive(list, joiner, result):
+    assert utils.list_to_string(list, joiner) == result
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("list, joiner, result", [
+    ([], ",", " "),
+    (["     123"], " ", "1,2,3"),
+    (["father", "mother", "daughter", "son"], ":", "father,mother,daughter,son")
+])
+def test_list_to_string_negative(list, joiner, result):
+    assert utils.list_to_string(list, joiner) == result
