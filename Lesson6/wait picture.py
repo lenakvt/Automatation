fe516c76631305dd1f65643ad6b07ed2d6b13deb
lenakvt <kilovataya.e.n@gmail.com>
2024-09-07 20:55:22 +0400

    Final add scripts

--- Lesson6/wait picture.py
+++ Lesson6/wait picture.py
@@ -10,7 +10,7 @@ driver.get('https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
 
 # Дождаться загрузки всех картинок
 pictures = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "[id=landscape]")))
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div#image-container.col-12.py-2 img#landscape")))
 
 # Получить значение атрибута src 3-й картинки и вывести в консольF
 src = driver.find_element(By.ID, "award").get_attribute("src")
